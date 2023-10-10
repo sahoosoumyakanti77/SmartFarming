85b26688ce9c8a2d65e222271e632d4e5c6d7660
sahoosoumyakanti77 <sahoosoumyakanti77@gmail.com>
2023-10-10 19:53:34 +0530

    first commit

---
+++ src/test/java/com/smartFarming/SmartFarmingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartFarming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFarmingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
